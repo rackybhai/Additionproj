f839544f9c5d95363b3809bdeedf28f9318ad8e9
rakesh <rakeshcheviti238@gmail.com>
2023-05-31 14:57:01 +0530

    nadigirikki vanakkam

---
+++ Additionoftwonumbers/src/com/rakesh/git/Addition.java
@@ -0,0 +1,13 @@
+package com.rakesh.git;
+
+public class Addition {
+
+	public static void main(String[] args) {
+		int num1 = 20;
+		int num2=30;
+		int sum = num1+num2;
+		System.out.println("addition of two numbers " + sum);
+
+	}
+
+}
---
+++ Additionoftwonumbers/src/com/rakesh/git/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author Rakesh
+ *
+ */
+module Additionoftwonumbers {
+}
